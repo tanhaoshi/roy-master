ff5745f14b499d7897ed5e7993cbf37ec297b074
tanhaoshi <tanhaoshi@qq.com>
2019-03-18 21:19:23 +0800

    Test the branch.

--- app/src/main/java/roy/application/master/proxy/IApi.java
+++ app/src/main/java/roy/application/master/proxy/IApi.java
@@ -7,6 +7,7 @@ import roy.application.master.annotation.Param;
 
 public interface IApi {
 
+    //test IApi
     @Dispense("EVENTBUS_TYPE_ACTION_MOVE")
     Observable getNetworkEntrance(@Param("name") String value , @Param("pwd") String pwd);
 }
